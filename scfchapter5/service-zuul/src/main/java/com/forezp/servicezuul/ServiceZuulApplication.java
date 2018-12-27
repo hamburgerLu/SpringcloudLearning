a1e8daa643f4eb74845d58873ae5d66a46459756
mfluhanbo <mfluhanbo@jd.com>
2018-12-27 19:03:27 +0800

    初次提交

---
+++ scfchapter5/service-zuul/src/main/java/com/forezp/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,23 @@
+package com.forezp.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableZuulServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
+
