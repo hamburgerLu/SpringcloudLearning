f642dad2cc1e4c0a4b129a4278d281986fe6ec66
mfluhanbo <mfluhanbo@jd.com>
2019-01-02 10:34:26 +0800

    sc高可用的eureka server，但是得改host

--- scfchapter10/eureka-server/src/test/java/com/forezp/eurekaserver/EurekaServerApplicationTests.java
+++ scfchapter10/eureka-server/src/test/java/com/forezp/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.forezp.servicezipkin;
+package com.forezp.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceZipkinApplicationTests {
+public class EurekaServerApplicationTests {
 
     @Test
     public void contextLoads() {
